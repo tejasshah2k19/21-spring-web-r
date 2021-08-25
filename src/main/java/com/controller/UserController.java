c1d2a65a31dd6133f06064cf8bdc3a4b95821440
Tejas <tejas14all@gmail.com>
2021-08-25 14:59:16 +0530

    first

---
+++ src/main/java/com/controller/UserController.java
@@ -0,0 +1,22 @@
+package com.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class UserController {
+
+	@RequestMapping(value = "/signup", method = RequestMethod.GET)
+	public String loadSignup() {
+		System.out.println("loadSignup()");
+
+		return "Signup";//jsp 
+	}
+	
+	//login 
+	
+	//welcome 
+	
+
+}
